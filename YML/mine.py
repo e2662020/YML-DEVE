f478fcc0a3775652f7fc8933cbec214fc2fec2e6
e2662020@outlook.com <e2662020@outlook.com>
2023-08-22 22:29:18 +0800

    Fix:Fix some bug.

--- YML/mine.py
+++ YML/mine.py
@@ -21,6 +21,19 @@ with open(myPATH+"azureAPI.txt", "r") as f:
     azureApiKey = f.read
 # if myOS == "Windows":
 
+def set_status(status: str):
+    print(status)
+
+
+def set_progress(progress: int):
+    if current_max != 0:
+        print(f"{progress}/{current_max}")
+
+
+def set_max(new_max: int):
+    global current_max
+    current_max = new_max
+
 callback = {
     "setStatus": set_status,
     "setProgress": set_progress,
