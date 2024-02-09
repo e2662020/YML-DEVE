3a859bce004e72a5db0eafe8b67441a0e048dc23
e2662020@outlook.com <e2662020@outlook.com>
2024-02-09 09:56:53 +0800

    fix sth.

--- YML/downloadFabric.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def installFabric(mcversion,mcDir):
-    path = os.getcwd()
-    path = path+"/fabricInstaller"
-    command = 'java -jar fabricInstaller.jar client -snapshot -dir "' + mcDir + '" -mcversion "' + mcversion + '"'
-    print(command)
-    os.system(command)
